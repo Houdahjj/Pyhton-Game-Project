b11a439f2028c3a7facb07148b0794dc5d2b95c9
Houdahjj <44894707+Houdahjj@users.noreply.github.com>
2020-10-23 10:13:03 +0200

    Update game.py

--- game.py
+++ game.py
@@ -15,23 +15,24 @@ class Game:
         #sprite de joueur pour collision
         self.all_player = pygame.sprite.Group()
         self.all_player.add(self.player)
+        #instance de l'évenement des projectiles et des haricots magiques
         self.projectile_event = ProjectileEvent(self)
         self.senzu_event = SenzuEvent(self)
-        self.pressed = {} #2 fluidité deplacement, coordonées qui saffichent quand on actionne une touche
+        self.pressed = {} # coordonées qui saffichent quand on actionne une touche
 
+        #si le joueur n'as plus de vie, la partie est terminé
     def game_over(self):
         self.player.health = self.player.max_health
         self.is_playing = False
+        #projectiles qui augmentent leurs vitesses
         self.projectile_event.all_projectiles = pygame.sprite.Group()
-        #self.heart_event.all_heart = pygame.sprite.Group()
         self.projectile_event.percent = 100
         self.projectile_event.b = 3
         self.projectile_event.percent_speed = 20
 
 
-
+    #mises à jour : lancements des projectiles, des haricots magiques, barre de vie
     def update(self,screen):
-
         self.player.update_health_bar(screen)
         # application image
         screen.blit(self.player.image, self.player.rect)
@@ -48,11 +49,8 @@ class Game:
 
         self.senzu_event.all_senzu.draw(screen)
 
-        #pv
-        #self.heart_event.all_heart.draw(screen)
-
         # Verifier si le joueur veux aller en haut ou en bas
-        # fluidité du déplacement !! -> 3/ On verifie où il veux aller et on actionne la fonction bouger
+        # fluidité du déplacement !! -> On verifie où il veux aller et on actionne la fonction bouger
         if self.pressed.get(pygame.K_UP) and self.player.rect.y > 10:
             self.player.move_up()
         elif self.pressed.get(pygame.K_DOWN) and self.player.rect.y + 90 < screen.get_height():
