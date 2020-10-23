169d0e7bfab95bc289a73edf2c79f02297c85181
Houdahjj <44894707+Houdahjj@users.noreply.github.com>
2020-10-23 11:40:33 +0200

    Update projectile.py

--- projectile.py
+++ projectile.py
@@ -4,6 +4,7 @@ import random
 
 class Projectile(pygame.sprite.Sprite):
 
+#instance des caracteristiques des projectiles : image, redimensions, vitesse, position des projectiles (aléatoire) 
     def __init__(self, projectile_event):
         super().__init__()
         self.image = pygame.image.load('comete.png')
@@ -14,14 +15,15 @@ class Projectile(pygame.sprite.Sprite):
         self.velocity = 5
         self.projectile_event = projectile_event
 
+#suppression d'un projectile (en cas de collisions)
     def remove(self):
         self.projectile_event.all_projectiles.remove(self)
 
-
-
+#detecter collision
     def check_collision(self, sprite, group):
         return pygame.sprite.spritecollide(sprite, group, False, pygame.sprite.collide_mask)
 
+#fonction qui lance les projectiles en fonction de la vitesse calculée, et les supprime à chaque collision avec le personnage
     def fall(self):
             self.rect.x -= self.velocity
             if self.rect.x < 0:
