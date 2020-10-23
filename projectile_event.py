2cc2737208bd0b08b044b4616e8da24f05011b6a
Houdahjj <44894707+Houdahjj@users.noreply.github.com>
2020-10-23 11:45:30 +0200

    Update projectile_event.py

--- projectile_event.py
+++ projectile_event.py
@@ -3,15 +3,17 @@ from projectile import Projectile
 
 class ProjectileEvent:
 
+#instancie la vitesse des projectiles, pour les initialiser dans le jeu ainsi que le groupe de projectiles
     def __init__(self, game):
         self.percent = 100
         self.percent_speed = 20
-        #groupe de projectile
+        #groupe de projectiles
         self.all_projectiles = pygame.sprite.Group()
         self.a = 0
         self.b = 3
         self.game = game
 
+ #nombre de projectiles qui vont être lancés en fonction d'un nombre a et b 
     def meteor_fall(self):
         for i in range(self.a, self.b):
             self.all_projectiles.add(Projectile(self))
@@ -19,21 +21,23 @@ class ProjectileEvent:
         self.percent_speed += 3
         # Pour augmenter le nombre de boules
         self.b += 1
-
+        
+ #reinitialiser les poucentages
     def reset_percent(self):
         self.percent = 0
 
+#si les pourcentages dépassent un quota (100), les projectiles sont lancés, les pourcentages servent de base pour que les projectiles se lancent, donc on remet à 0 à chaque fois
     def attempt_fall(self):
         if self.loaded():
             self.meteor_fall()
-
-            # ICI QUE JE DOIS AUGMENTER LA VELOCITY
             self.reset_percent()
-
+            
+#on actualise pour boucler le cycle pourcentage/lancement de projectiles
     def update(self):
         self.add_percent()
         self.attempt_fall()
 
+#vitesse d'évolution des pourcents
     def add_percent(self):
         self.percent += self.percent_speed / 100
 
