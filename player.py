12b79756cf8afd60c9cb43617a32df0befe923f7
Houdahjj <44894707+Houdahjj@users.noreply.github.com>
2020-10-23 10:19:22 +0200

    Update player.py

--- player.py
+++ player.py
@@ -25,6 +25,7 @@ class Player(pygame.sprite.Sprite):         #sprite pour que ça soit un composa
     def move_down(self):
         self.rect.y += self.velocity
 
+#fonction de mise à jour de la barre de vie (appelée dans la classe game),dessine une barre noir et une barre jaune qui représenteront le pourcentage de points de vie
     def update_health_bar(self, surface):
         bar_color = (255, 247, 0)
         bar_position = [970, 10, self.max_health, 10]
@@ -33,12 +34,13 @@ class Player(pygame.sprite.Sprite):         #sprite pour que ça soit un composa
         pygame.draw.rect(surface, black_bar, bar_position)
         pygame.draw.rect(surface, bar_color, black_bar_position)
 
-
+#dommages qui sont engendrés, si il n'y a plus de points de vie, on passe à la fonction game_over
     def damage(self, amount):
         self.health -= amount
         if self.health == 0:
             self.game.game_over()
 
+#points de vie qui sont augmentés avec les haricots magiques, jusqu'à que la barre soit pleine
     def senzu(self, amount):
         while not self.health == self.max_health:
             self.health += amount
